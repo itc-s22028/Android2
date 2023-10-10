6254e90c4298f8d49318f921542cfdf17c354860
itc-s22028 <s22028@std.it-college.ac.jp>
2023-10-10 12:26:16 +0900

    chap11 ktorクライアントを使ったサンプル完成

--- KtorSample/app/src/main/java/jp/ac/it_college/std/s22028/ktor_sample/MainActivity.kt
+++ KtorSample/app/src/main/java/jp/ac/it_college/std/s22028/ktor_sample/MainActivity.kt
@@ -2,9 +2,41 @@ package jp.ac.it_college.std.s22028.ktor_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.UiThread
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.cio.endpoint
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.url
+import io.ktor.serialization.kotlinx.json.json
 import jp.ac.it_college.std.s22028.ktor_sample.databinding.ActivityMainBinding
 import jp.ac.it_college.std.s22028.ktor_sample.databinding.RowBinding
+import jp.ac.it_college.std.s22028.ktor_sample.model.WeatherInfo
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
+
+private val ktorClient = HttpClient(CIO) {
+    engine {
+        endpoint{
+            connectTimeout = 1000
+            requestTimeout = 1000
+            socketTimeout = 1000
+        }
+    }
+    install(ContentNegotiation) {
+        json(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            }
+        )
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     companion object{
@@ -21,9 +53,31 @@ class MainActivity : AppCompatActivity() {
 
         binding.rvCityList.apply {
             adapter = CityAdapter {
-
+                getWeatherInfo(it.q)
             }
             layoutManager = LinearLayoutManager(context)
+        }
+    }
+    @UiThread
+    private fun getWeatherInfo(q: String) {
+        lifecycleScope.launch {
+            //データ取得部分
+            val url = "$WEATHER_INFO_URL&q=$q&appid=$APP_ID"
+            val result = ktorClient.get{
+                url(url)
+            }.body<WeatherInfo>()
+
+            //取得したデータをUIに反映
+            result.run {
+                binding.tvWeatherTelop.text = getString(R.string.tv_telop, cityName)
+                binding.tvWeatherDesc.text = getString(
+                    R.string.tv_desc,
+                    weather[0].description,
+                    coordinates.longitude,
+                    coordinates.latiude,
+                )
+            }
+
         }
     }
 }
