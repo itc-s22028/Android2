32c02f4698517cde86e33e07213ed527d9079fa9
itc-s22028 <s22028@std.it-college.ac.jp>
2023-10-10 11:24:40 +0900

    chap11 11.7 view

---
+++ KtorSample/app/src/main/java/jp/ac/it_college/std/s22028/ktor_sample/MainActivity.kt
@@ -0,0 +1,29 @@
+package jp.ac.it_college.std.s22028.ktor_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import jp.ac.it_college.std.s22028.ktor_sample.databinding.ActivityMainBinding
+import jp.ac.it_college.std.s22028.ktor_sample.databinding.RowBinding
+
+class MainActivity : AppCompatActivity() {
+    companion object{
+        private const val WEATHER_INFO_URL =
+            "https://api.openweathermap.org/data/2.5/weather?lang=ja"
+        private const val APP_ID = BuildConfig.APP_ID
+    }
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvCityList.apply {
+            adapter = CityAdapter {
+
+            }
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+}
